3abf836efb2327034b933b69fd05bb19b952f782
vladimoroshan <vladimoroshan0022@gmail.com>
2017-10-02 18:06:07 +0300

    Create scan.py

---
+++ word_scanner/scan.py
@@ -0,0 +1,38 @@
+def scanner(raw_sentence):
+	lexicon = {
+		'north': 'direction', 
+		'south': 'direction', 
+		'east': 'direction', 
+		'west': 'direction', 
+		'down': 'direction', 
+		'up': 'direction', 
+		'left': 'direction', 
+		'right': 'direction', 
+		'back': 'direction',
+		'go': 'verb', 
+		'stop': 'verb',
+		'kill': 'verb',
+		'eat': 'verb',
+		'the': 'stop',
+		'in': 'stop',
+		'of': 'stop', 
+		'from': 'stop',
+		'at': 'stop',
+		'it': 'stop',
+		'door': 'noun',
+		'bear': 'noun',
+		'princess': 'noun',
+		'cabinet': 'noun'
+	}
+
+	words = raw_sentence.lower().split()
+	list_of_tuples = []
+
+	for word in words:
+		if word.isdigit():
+			list_of_tuples.append(('number', int(word)))
+		else:
+			corresponding_word = lexicon.get(word, 'error')
+			list_of_tuples.append((corresponding_word, word))
+
+	return list_of_tuples
