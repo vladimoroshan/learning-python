fa1bcc2af11d68e04c468b1df3ccf136f9da16b4
vladimoroshan <vladimoroshan0022@gmail.com>
2017-10-09 12:14:21 +0300

    Update Twilio_send_sms.py

--- Twilio_send_sms.py
+++ Twilio_send_sms.py
@@ -3,13 +3,13 @@
 
 from twilio.rest import Client
 
-ACCOUNT_SID = 'AC1f0ccba8ff29f09818bf57480f4e1218'
-AUTH_TOKEN = 'e931a9e357bcdd00fbb1744eb9941f36'
+ACCOUNT_SID = 'ACxxxxxxxxxxxxxxxxxxxxxxxxx'
+AUTH_TOKEN = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
 client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
-myTwilioNumber = '+15017250604'
-myCellPhone = '+380996078737'
+myTwilioNumber = '+*********************'
+myCellPhone = '+*******************''
 text = 'Hello from Python'
 
 message = client.messages.create(from_=myTwilioNumber, to=myCellPhone, body=text)
